ff63dec4a338f25f2847a4f10b2c5ea15e9bd791
yokomaru <cabochetoganlan@gmail.com>
2023-12-16 01:10:52 +0900

    fix: リファクタリング

--- osero.rb
+++ osero.rb
@@ -2,7 +2,7 @@ class Osero
   WHITE = "○"
   BLACK = "●"
   BLANK = "."
-  GRID_BOARD_NUM = 6
+  GRID_BOARD_NUM = 4
   CHECKING_DIRECTIONS = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 0], [0, 1], [1, -1], [1, 0], [1, 1]]
 
   def game_start
@@ -14,21 +14,17 @@ class Osero
     while grid_board.flatten.include?(".") do
       puts "あなたは○です。置きたいところを入力してください"
 
-      # ユーザーのキーワード入力を待機
-      input_val = gets
-
       # 入力値を配列に変換
-      input_num = input_val.chomp.split(",").map(&:to_i)
+      input_num_array = convert_input_val_to_input_num_array(gets)
 
       # 入力値チェック
-      # TODO:既に碁が置かれている場所を指定していないか
-      # TODO:指定の仕方が間違っていないか
-      # TODO:integer以外が入力されていないか
-      next if input_num.size == 1
+      next puts "数字をカンマ区切りで2つ入力してね" if check_input_num_array_size(input_num_array)
+      next puts "碁盤の範囲も数字を入力してね" if check_input_num_array_range(input_num_array)
+      next puts "数字を入力してね" if check_input_num_array_content(input_num_array)
 
-      if check_if_disk_can_put(grid_board, input_num, WHITE, BLACK)
-        grid_board[input_num[0]][input_num[1]] = WHITE
-        myturn_grid_board = reverse_all_disks(grid_board, input_num, WHITE, BLACK)
+      if check_if_disk_can_put(grid_board, input_num_array, WHITE, BLACK)
+        grid_board[input_num_array[0]][input_num_array[1]] = WHITE
+        myturn_grid_board = reverse_all_disks(grid_board, input_num_array, WHITE, BLACK)
       else
         puts "そこには置けません"
         next
@@ -38,25 +34,24 @@ class Osero
       preview_grid_board(myturn_grid_board)
 
       puts "相手(●)のターンです。"
+
       # 敵が置ける場所を探す
       enemy_turn_grid_board = check_enemy_can_put(myturn_grid_board)
+
       # 碁盤の再表示
       preview_grid_board(enemy_turn_grid_board)
     end
 
-    puts "ゲーム終了"
-    puts "あなたの数 #{grid_board.flatten.count(WHITE)}"
-    puts "相手の数 #{grid_board.flatten.count(BLACK)}"
-    puts "#{grid_board.flatten.count(WHITE) > grid_board.flatten.count(BLACK) ? "あなたの勝ち" : "あなたの負け"}"
+    puts game_result(grid_board)
   end
 
   def make_field
     grid_board = Array.new(GRID_BOARD_NUM) { Array.new(GRID_BOARD_NUM, BLANK) }
     # 初期位置に碁を交互に置く
     grid_board[2][2] = WHITE
-    grid_board[2][3] = BLACK
-    grid_board[3][2] = BLACK
-    grid_board[3][3] = WHITE
+    grid_board[1][2] = BLACK
+    grid_board[2][1] = BLACK
+    grid_board[1][1] = WHITE
     grid_board
   end
 
@@ -66,10 +61,10 @@ class Osero
     end
   end
 
-  def reverse_all_disks(grid_board, input_num, my_color, enemy_color)
+  def reverse_all_disks(grid_board, input_num_array, my_color, enemy_color)
     CHECKING_DIRECTIONS.each do |c|
       my_reverse_array = []
-      x, y = input_num
+      x, y = input_num_array
 
       if grid_board.dig(x + c[0], y + c[1]) == enemy_color
         while x + c[0].between?(0, GRID_BOARD_NUM - 1) && y + c[1].between?(0, GRID_BOARD_NUM - 1)
@@ -102,9 +97,9 @@ class Osero
     grid_board
   end
 
-  def check_if_disk_can_put(grid_board, input_num, my_color, enemy_color)
+  def check_if_disk_can_put(grid_board, input_num_array, my_color, enemy_color)
     CHECKING_DIRECTIONS.each do |c|
-      x, y = input_num
+      x, y = input_num_array
 
       if grid_board.dig(x + c[0], y + c[1]) == enemy_color
         while x + c[0].between?(0, GRID_BOARD_NUM - 1) && y + c[1].between?(0, GRID_BOARD_NUM - 1)
@@ -121,10 +116,9 @@ class Osero
     false
   end
 
-  def check_if_disk_can_put(grid_board, input_num, my_color, enemy_color)
+  def check_if_disk_can_put(grid_board, input_num_array, my_color, enemy_color)
     CHECKING_DIRECTIONS.each do |c|
-      x = input_num[0]
-      y = input_num[1]
+      x, y = input_num_array
       if grid_board.dig(x + c[0], y + c[1]) == enemy_color
         x += c[0]
         y += c[1]
@@ -143,10 +137,10 @@ class Osero
     false
   end
 
-  def reverse_all_disks(grid_board, input_num, my_color, enemy_color)
+  def reverse_all_disks(grid_board, input_num_array, my_color, enemy_color)
     CHECKING_DIRECTIONS.each do |c|
       my_reverse_array = []
-      x, y = input_num
+      x, y = input_num_array
 
       if grid_board.dig(x + c[0], y + c[1]) == enemy_color
         while (x + c[0]).between?(0, GRID_BOARD_NUM - 1) && (y + c[1]).between?(0, GRID_BOARD_NUM - 1)
@@ -177,7 +171,29 @@ class Osero
     end
     return grid_board
   end
-end
 
-osero = Osero.new
-osero.game_start
+  def game_result(g)
+    result << "ゲーム終了"
+    result << "あなたの数 #{g.flatten.count(WHITE)}"
+    result << "相手の数 #{g.flatten.count(BLACK)}"
+    # 引き分けだった場合
+    result << "#{g.flatten.count(WHITE) > g.flatten.count(BLACK) ? "あなたの勝ち" : "あなたの負け"}"
+    result.join("\n")
+  end
+
+  def check_input_num_array_size(input_num_array)
+    input_num_array.size < 2 || input_num_array.size > 2
+  end
+
+  def check_input_num_array_range(input_num_array)
+    input_num_array[0] > GRID_BOARD_NUM - 1 || input_num_array[1] > GRID_BOARD_NUM - 1 || input_num_array[0] < 0 || input_num_array[1] < 0
+  end
+
+  def check_input_num_array_content(input_num_array)
+    input_num_array[0].nil? || input_num_array[1].nil?
+  end
+
+  def convert_input_val_to_input_num_array(input_val)
+    input_val.chomp.split(",").map(&:to_i)
+  end
+end
