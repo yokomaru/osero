b55abbd658230e4eb93efa855e3736689b5f0efc
yokomaru <cabochetoganlan@gmail.com>
2023-12-19 00:07:37 +0900

    fix: itで書いている部分をcontextとexampleに修正

--- spec/osero_spec.rb
+++ spec/osero_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
-require './osero'
+require './lib/osero'
 
 RSpec.describe Osero do
 
@@ -9,44 +9,58 @@ RSpec.describe Osero do
 
   describe 'method' do
     describe 'make_field' do
-      it 'work' do
+      example '4x4の配列を返すこと' do
         expect(osero.make_field).to eq [[".", ".", ".", "."], [".", "○", "●", "."], [".", "●", "○", "."], [".", ".", ".", "."]]
       end
     end
 
     describe 'convert_input_val_to_input_num_array' do
-      it 'work' do
-        expect(osero.convert_input_val_to_input_num_array("0, 2")).to eq [0, 2]
+      context '0, 2という入力の場合' do
+        example '[0,2]という配列を返すこと' do
+          expect(osero.convert_input_val_to_input_num_array("0, 2")).to eq [0, 2]
+        end
       end
     end
 
     describe 'check_input_num_array_size' do
-      it 'work' do
-        expect(osero.check_input_num_array_size([0, 2])).to eq false
+      context '[0, 2]' do
+        example 'チェックに通らないこと' do
+          expect(osero.check_input_num_array_size([0])).to eq true
+        end
       end
 
-      it 'not work' do
-        expect(osero.check_input_num_array_size([0])).to eq true
+      context '[0, 2]' do
+        example 'チェックに通ること' do
+          expect(osero.check_input_num_array_size([0, 2])).to eq false
+        end
       end
     end
 
     describe 'check_input_num_array_range' do
-      it 'work' do
-        expect(osero.check_input_num_array_range([0, 2])).to eq false
+      context '[0, 2]' do
+        example 'チェックに通ること' do
+          expect(osero.check_input_num_array_range([0, 2])).to eq false
+        end
       end
 
-      it 'not work' do
-        expect(osero.check_input_num_array_range([-1, -1])).to eq true
+      context '[-1, -1]' do
+        example 'チェックに通らないこと' do
+          expect(osero.check_input_num_array_range([-1, -1])).to eq true
+        end
       end
     end
 
     describe 'check_input_num_array_content' do
-      it 'work' do
-        expect(osero.check_input_num_array_content([0, 2])).to eq false
+      context '[0, 2]' do
+        example 'チェックに通ること' do
+          expect(osero.check_input_num_array_content([0, 2])).to eq false
+        end
       end
 
-      it 'not work' do
-        expect(osero.check_input_num_array_content([nil, 1])).to eq true
+      context '[nil, 1]' do
+        example 'チェックに通らないこと' do
+          expect(osero.check_input_num_array_content([nil, 1])).to eq true
+        end
       end
     end
   end
